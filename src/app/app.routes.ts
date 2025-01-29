798079346906003fcf8360a49954f635c94ba738
JBeckintosh <James-beck@live.com>
2025-01-29 07:49:22 +0000

    trying the fail over with a 404.html file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BopItComponent } from './bop-it/bop-it.component';
 import { SecretSantaComponent } from './secret-santa/secret-santa.component';
 import { TarotGameComponent } from './tarot-game/tarot-game.component';
@@ -21,9 +20,5 @@ export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    },
-    {
-        path: '**',
-        component: PageNotFoundComponent
     }
 ];
